2bd6d0dd4b2288ff8bd634daf08335edbf6da0bc
ashan <ashannon1990@gmail.com>
2019-11-07 21:30:47 -0500

    added focus to last person on list

--- src/components/Persons/Person/index.jsx
+++ src/components/Persons/Person/index.jsx
@@ -7,13 +7,25 @@ import withClass from '../../../hoc/withClass';
 import classes from './style.css';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus()
+    }
+    
     render() {
         return (
             <Fragment>
-                <p key={'i1'} onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p key={"i2"}>{this.props.children}</p>
+                <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p key="i2">{this.props.children}</p>
                 <input
-                    key={'i3'}
+                    key='i3'
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type='text'
                     onChange={this.props.changed}
                     value={this.props.name}
